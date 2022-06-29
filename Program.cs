33bec759a2c3228246115550e9a8c2f3816d47c2
Yung.Energy <morskojroma@gmail.com>
2022-06-29 22:31:33 +0300

    Task1

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿int randomNumber = new Random().Next(100, 999);
+int firstDigit = randomNumber / 100;
+int secondDigit = randomNumber / 10;
+int needDigit = secondDigit - (firstDigit * 10);
+Console.WriteLine($"{randomNumber} - {needDigit}");
